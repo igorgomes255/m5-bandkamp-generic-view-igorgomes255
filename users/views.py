62040502f5e687209508d0332b9d4b336c9b7364
Igor Gomes <igorgomes255@gmail.com>
2022-12-14 21:41:14 -0300

    finalizando parte 3

--- users/views.py
+++ users/views.py
@@ -1,10 +1,8 @@
-from rest_framework.views import APIView, Request, Response, status
-from rest_framework.generics import CreateAPIView
 from .models import User
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from .serializers import UserSerializer
-from django.shortcuts import get_object_or_404
 from .permissions import IsAccountOwner
+from .serializers import UserSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 class UserView(CreateAPIView):
@@ -12,44 +10,9 @@ class UserView(CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserDetailView(APIView):
+class UserDetailView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAccountOwner]
 
-    def get(self, request: Request, pk: int) -> Response:
-        """
-        Obtençao de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        serializer = UserSerializer(user)
-
-        return Response(serializer.data)
-
-    def patch(self, request: Request, pk: int) -> Response:
-        """
-        Atualização de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data)
-
-    def delete(self, request: Request, pk: int) -> Response:
-        """
-        Deleçao de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        user.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
